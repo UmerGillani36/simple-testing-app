9bcf5b1dbe217bdcfd2fd78140af824728fd3239
UmerGillani36 <syedomershahgillani@gmail.com>
2023-08-14 14:40:16 +0500

    not best practice alternative of mocking

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -11,4 +11,29 @@ test('it shows two inputs and a button', async()=>{
 
     expect(inputs).toHaveLength(2);
     expect(button).toBeInTheDocument();
-})
+});
+
+test('it calls onUserAdd when the form is submitted', ()=>{
+    const argList = [];
+    const callback = (...args)=>{
+        argList.push(args);
+    }
+
+    render(<UserForm onUserAdd={callback}/>);
+
+    const [nameInput, emailInput] = screen.getAllByRole('textbox');
+    const button = screen.getByRole('button');
+
+    user.click(nameInput);
+    user.keyboard("umer");
+
+    user.click(emailInput);
+    user.keyboard("umer@gmail.com");
+
+    user.click(button);
+
+    expect(argList).toHaveLength(1);
+    expect(argList[0][0]).toEqual({name:'umer',email:'umer@gmail.com'});
+    
+
+});
