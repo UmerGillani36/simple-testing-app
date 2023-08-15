7c7d38e6c93e33a5903670227989874de263db1b
UmerGillani36 <syedomershahgillani@gmail.com>
2023-08-15 16:10:06 +0500

    query selector all

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,20 +1,21 @@
-import { render, screen,within } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import UserList from './UserList';
 
+test('render one row per user', () => {
+  const users = [
+    { name: 'umer', email: 'umer@gmail.com' },
+    { name: 'ahsan', email: 'ahsan@gmail.com' },
+  ];
+  const { container } = render(<UserList users={users} />);
 
-test('render one row per user', ()=>{
-    const users = [
-        {name:'umer', email:'umer@gmail.com'},
-        {name:'ahsan', email:'ahsan@gmail.com'},
-    ]
-    render(<UserList users={users}/>)
+  // screen.logTestingPlaygroundURL();
+  // const rows = screen.getAllByRole('row');
+  // const rows = within(screen.getByTestId('users')).getAllByRole('row');
 
-    // screen.logTestingPlaygroundURL();
-    // const rows = screen.getAllByRole('row');
-    const rows = within(screen.getByTestId('users')).getAllByRole('row');
+  const rows = container.querySelectorAll('tbody tr');
 
-    expect(rows).toHaveLength(2);
-})
+  expect(rows).toHaveLength(2);
+});
 
 // test('render the email and name of each user', ()=>{
 //     render(<UserList/>)
