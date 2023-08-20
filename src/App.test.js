1295d99df57dcc3e579aff11130280501d2c594f
UmerGillani36 <syedomershahgillani@gmail.com>
2023-08-20 13:22:52 +0500

    whole app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import user from '@testing-library/user-event';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('can receive a new user and show it on list',()=>{
+    render(<App/>);
+
+    const nameInput = screen.getByRole('textbox',{name: /name/i});
+    const emailInput = screen.getByRole('textbox',{name:/email/i});
+    
+    const button = screen.getByRole('button');
+
+    user.click(nameInput);
+    user.keyboard("umer");
+
+    user.click(emailInput);
+    user.keyboard("umer@gmail.com");
+
+    user.click(button);
+
+    // screen.debug()
+    const name = screen.getByRole('cell', { name: 'umer' });
+    const email = screen.getByRole('cell', { name: 'umer@gmail.com' });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+})
