77152b373637223a1c84860edd4e16b45d9941fc
UmerGillani36 <syedomershahgillani@gmail.com>
2023-08-20 13:11:31 +0500

    beforeEach and reusalble comp data

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,28 +1,37 @@
 import { render, screen, within } from '@testing-library/react';
 import UserList from './UserList';
 
-test('render one row per user', () => {
+const renderComponent=()=>{
   const users = [
     { name: 'umer', email: 'umer@gmail.com' },
     { name: 'ahsan', email: 'ahsan@gmail.com' },
   ];
-  const { container } = render(<UserList users={users} />);
+   render(<UserList users={users} />);
 
+   return {
+    users,
+   }
+}
+// not best for rendering component by react testing library
+
+// beforeEach(()=>{
+//     render(<UserList users={users} />);
+// })
+
+test('render one row per user', () => {
+
+  renderComponent();
   // screen.logTestingPlaygroundURL();
   // const rows = screen.getAllByRole('row');
-  // const rows = within(screen.getByTestId('users')).getAllByRole('row');
+  const rows = within(screen.getByTestId('users')).getAllByRole('row');
 
-  const rows = container.querySelectorAll('tbody tr');
+  // const rows = container.querySelectorAll('tbody tr');
 
   expect(rows).toHaveLength(2);
 });
 
 test('render the email and name of each user', () => {
-  const users = [
-    { name: 'umer', email: 'umer@gmail.com' },
-    { name: 'ahsan', email: 'ahsan@gmail.com' },
-  ];
-  render(<UserList users={users} />);
+const {users} = renderComponent();
 
   for (let user of users) {
     const name = screen.getByRole('cell', { name: user.name });
